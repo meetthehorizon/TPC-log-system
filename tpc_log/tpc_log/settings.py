1654574e24b1af1076af9b76f2436bcfff124826
Kshitij Sharma <kshitij.sharma.cse22@itbhu.ac.in>
2023-11-07 23:58:53 +0530

    Update settings.py

--- tpc_log/tpc_log/settings.py
+++ tpc_log/tpc_log/settings.py
@@ -77,13 +77,17 @@ WSGI_APPLICATION = 'tpc_log.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        'ENGINE': config("DB_ENGINE"),
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
+        'PORT': config("DB_PORT")
     }
 }
+
 
 
 # Password validation
